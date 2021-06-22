cffc9a3ae5afdfa78c3361f0925f0e2756b04abb
André Vitor de Freitas Adams <55411040+andrevitor103@users.noreply.github.com>
2021-06-21 21:05:06 -0300

    Update listDespesas.api.php

--- api/listDespesas.api.php
+++ api/listDespesas.api.php
@@ -5,21 +5,23 @@
 	include '../Config.php';
 	include '../Despesa.class.php';
 
+
+	// echo 'aqui';
+
 	// echo $_SESSION['id_user'];
 
 	$despesa = new Despesa($pdo);
+	
+	// print_r($despesa->selectDt(null, $_SESSION['id_user']));
+
 	if(isset($_GET['id'])){
 		$despesa = $despesa->selectDt($_GET['id'], $_SESSION['id_user']);
 	}else if(isset($_GET['filters'])){
-
-		<<<HTML
-		<br>
-		<h4>{$_GET['filters']}<h4>
-HTML;
 		$despesa = $despesa->selectDtFilter($_GET['filters'], $_SESSION['id_user']);
 	}else{
 		$despesa = $despesa->selectDt(null, $_SESSION['id_user']);
 	}
+
 	$despesa = json_encode($despesa);
 	echo $despesa;
 
